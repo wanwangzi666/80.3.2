273235781d9369a7336a646dc1f2217e3f0cdb30
b a <ab@wanwangzi.onmicrosoft.com>
2022-04-21 13:19:19 +0800

    添加项目文件。

---
+++ 80.3.2/80.3.2.cpp
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+int main()
+{
+    int r, k, s, l;
+    printf("半径");
+    scanf_s("%d", &r);
+    printf("K");
+    scanf_s("%d", &k);
+
+    s = 3.14 * (r * r) ;
+    l = 2 * 3.14 * r;
+    printf("s=%lf, l=%lf\n", s, l);
+    if (k == 1)
+    {
+         printf("s=%f\n", &s);
+    }
+    else
+    {
+        if (k == 2)
+        {
+            scanf_s("%d", &l);
+        }
+        else
+        {
+            if (k == 3)
+            {
+                scanf_s("%d,%d", &s, &r);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
